c250930c0bc0100bb6a6b2e95b69aa4a515e3b3d
Matt Ivan <matt@mattivan.com>
2022-01-02 12:33:03 -0800

    TreasuryFix

---
+++ GreaseMoney/TreasuryFix.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name     TreasuryFix
+// @version  1.01
+// @grant    none
+// @include        https://www.treasurydirect.gov/RS/*
+// ==/UserScript==
+
+
+for (let e of document.getElementsByClassName("pwordinput")) { e.removeAttribute('readonly'); }
