2362fa3d55059db813355449de0a4c598dadd5d2
Matt Ivan <matt@mattivan.com>
2022-01-02 12:33:29 -0800

    Delete TreasuryFix.user.js

--- GreaseMoney/TreasuryFix.user.js
+++
@@ -1,9 +0,0 @@
-// ==UserScript==
-// @name     TreasuryFix
-// @version  1.01
-// @grant    none
-// @include        https://www.treasurydirect.gov/RS/*
-// ==/UserScript==
-
-
-for (let e of document.getElementsByClassName("pwordinput")) { e.removeAttribute('readonly'); }
