5b19727b6cebda7422ff9892622aeeccd031ec57
Matt Ivan <matt@mattivan.com>
2022-01-02 13:08:57 -0800

    Update TreasuryFix.user.js

--- TaperMonkey/TreasuryFix.user.js
+++ TaperMonkey/TreasuryFix.user.js
@@ -1,1 +1,8 @@
-temp
+// ==UserScript==
+// @name     TreasuryFix
+// @version  1.01
+// @grant    none
+// @include        https://www.treasurydirect.gov/RS/*
+// ==/UserScript==
+
+for (let e of document.getElementsByClassName("pwordinput")) { e.removeAttribute('readonly'); }
